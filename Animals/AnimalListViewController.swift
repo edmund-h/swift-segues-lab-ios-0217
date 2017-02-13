56e2da774036b19b665ff6fc1f467c3acf8ff87e
edmund-h <447ght@gmail.com>
2017-02-13 17:59:39 -0500

    done but still not used to all the things

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -9,4 +9,29 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    let animals = ["🐶","🐱","🐹","🐭","🐰","🦊","🐻","🐼","🦁","🐨","🐯","🦁","🐮","🐷","🐸","🐵","🐙","🐔","🐴","🦄", "👽"]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+        let myAnimal = animals[indexPath.row]
+        cell.textLabel?.text = myAnimal
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        guard segue.identifier == "AnimalDetailView" else {return}
+        if let dest = segue.destination as? AnimalViewController,
+            let indexPath = tableView.indexPathForSelectedRow{
+            dest.myAnimal = animals [indexPath.row]
+        }
+    }
 }
--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,41 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    var myAnimal = ""
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+
+    override func viewWillAppear(_ animated: Bool) {
+        emojiLabel.text = myAnimal
+        
+        navigationItem.title = "???"
+        textLabel.text = "Unknown Animal!"
+        guard let animalDesc = animalDict[myAnimal]else{ return }
+        textLabel.text = animalDesc
+        navigationItem.title = animalDesc
+    }
 }
+
+fileprivate let animalDict = [
+    "🐶": "Dog",
+    "🐱": "Cat",
+    "🐹": "Hamster",
+    "🐭": "Mouse",
+    "🐰": "Bunny",
+    "🦊": "Fox",
+    "🐻": "Bear",
+    "🐼": "Panda",
+    "🦁": "Lion",
+    "🐨": "Koala",
+    "🐯": "Tiger",
+    "🐮": "Cow",
+    "🐷": "Pig",
+    "🐸": "Frog",
+    "🐵": "Monkey",
+    "🐙": "Octopus",
+    "🐔": "Chicken",
+    "🐴": "Horse",
+    "🦄": "Unicorn"
+]
